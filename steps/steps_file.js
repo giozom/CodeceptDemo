3975b8c1c4d3cc89cecafbfc30dfcb7fb001eb74
Giovanni Arsenius <garsenius@odecee.com.au>
2016-12-14 10:00:31 +1100

    Updated users.yaml and steps_file to use user declared om yaml file

--- steps/steps_file.js
+++ steps/steps_file.js
@@ -1,6 +1,8 @@
 
 'use strict';
-// in this file you can append custom step methods to 'I' object
+let yaml = require('js-yaml');
+let fs   = require('fs');
+var users = yaml.safeLoad(fs.readFileSync('./conf/webdriverio/users.yaml', 'utf8'));
 
 module.exports = function() {
   return actor({
@@ -12,13 +14,15 @@ module.exports = function() {
           this.clearCookie();
           this.seeInTitle('Login');
 
-          /* Comment: Using users in yaml. Enable this if reading from users.yaml file
-          //this.fillField('//input[@name="username"]', users[name].username);
-          //this.fillField('//input[@name="username"]', users[name].username);
+          //Using user from users.yaml file
+          this.fillField('//input[@placeholder="Email"]', users[name].email);
+          this.fillField('//input[@name="password"]', users[name].password);
+
+          /*Or Alternatively, you can specify the locators and value
+          //this.fillField('//input[@name="username"]', 'user@phptravels.com');
+          //this.fillField('//input[@name="password"]', 'demouser');
           */
 
-          this.fillField('//input[@name="username"]', 'user@phptravels.com');
-          this.fillField('//input[@name="password"]', 'demouser');
           this.pressKey('Enter');
           this.wait(3);
           this.seeCurrentUrlEquals('http://phptravels.net/account/');
