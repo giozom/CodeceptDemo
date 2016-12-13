30734e85621c751dac955894db1431b6ffc954d2
Giovanni Arsenius <garsenius@odecee.com.au>
2016-12-13 11:38:38 +1100

    updated acceptance.conf.js

--- conf/webdriverio/acceptance.conf.js
+++ conf/webdriverio/acceptance.conf.js
@@ -1,27 +1,28 @@
 let config = {
-  tests: "./test/*.js",
-  helpers: {
-    WebDriverIO: {
-      url: 'http://phptravels.net',
-      driver: 'direct',
-      browser: 'firefox',
-      rootElement: 'html',
-      coloredLogs: true,
+    tests: "./test/*_test.js",
+    timeout: 10000,
+    output: "./output",
+    helpers: {
+        WebDriverIO: {
+            url: 'http://phptravels.net',
+            driver: 'direct',
+            browser: 'firefox',
+            rootElement: 'html',
+            coloredLogs: true,
+        }
+    },
+    include: {
+        I: "./steps/steps_file.js",
+        loginPage: "./pages/LoginPage.js",
+        bookingPage: "./pages/BookingPage.js"
+    },
+    mocha: {
+        reporterOptions: {
+            mochaFile: "./reports/results.xml"
+        }
     }
-  },
-  include: {
-    I: "./steps/steps.js",
-    loginPage: "./pages/LoginPage.js",
-  },
-  mocha: {
-    reporterOptions: {
-      mochaFile: "./reports/webdriverio.xml"
-    }
-  }
 };
 
-if(process.env.NODE_ENV === 'ci') {
-    config.helpers.WebDriverIO.url = 'http://phptravels.net';
-}
+
 
 module.exports = config;
