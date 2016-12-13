b51138956f7678f79178f12e7a340cf23c6e2ceb
Giovanni Arsenius <garsenius@odecee.com.au>
2016-12-13 11:40:23 +1100

    Updated steps_file.js to add logOut function

--- steps/steps_file.js
+++ steps/steps_file.js
@@ -11,16 +11,24 @@ module.exports = function() {
           this.resizeWindow('maximize');
           this.clearCookie();
           this.seeInTitle('Login');
+
+          /* Comment: Using users in yaml. Enable this if reading from users.yaml file
           //this.fillField('//input[@name="username"]', users[name].username);
+          //this.fillField('//input[@name="username"]', users[name].username);
+          */
+
           this.fillField('//input[@name="username"]', 'user@phptravels.com');
-          //this.fillField('//input[@name="username"]', users[name].username);
           this.fillField('//input[@name="password"]', 'demouser');
           this.pressKey('Enter');
-          this.wait(2);
-          this.click("Login");
-          this.seeInCurrentUrl('/account/');
-          this.see('John');
+          this.wait(3);
+          this.seeCurrentUrlEquals('http://phptravels.net/account/');
+          this.seeInTitle('My Account');
+          this.see('Hi, John Smith');
       },
+
+      logOut: function () {
+          this.amOnPage('http://phptravels.net/account/logout/')
+      }
 
   });
 }
